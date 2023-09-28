a021faf416c0e5e0378b05397f45e42bc3705d29
Yash <sharmayash380@gmail.com>
2023-09-29 00:34:22 +0400

    cors error fixed

--- api/app.js
+++ api/app.js
@@ -29,7 +29,7 @@ const server = app.listen(process.env.PORT || 8080, () => {
 
 const io = require("socket.io")(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://note-share-gamma.vercel.app",
     method: ["GET", "POST"],
   },
 });
